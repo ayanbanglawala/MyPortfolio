7bf11c78d8f0962a943578d6307aa2358e0bd1da
Ayan <ayanchhipa2278@gmail.com>
2024-12-11 11:10:16 +0530

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MyPortfolio/', // Set the base path to match your repository name
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-tilt'], // Externalize react-tilt
+    },
+  },
 });
